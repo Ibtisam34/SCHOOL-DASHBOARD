5240b4988424c6c44620645cd5f78d60acc6d029
unknown <Riyanagwl@gmail.com>
2023-03-23 12:42:01 +0300

    registration-backend

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  class UsersController < ApplicationController 
+    def create
+      user = user.new (users_params)
+      if user.save
+        render json : {success:true}
+      else
+        render json = {errors:user.errors.full_messages},
+        status : unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+  end
+end
