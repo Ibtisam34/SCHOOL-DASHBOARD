261612485b5df6d5b02d43a1340fc68fd9c81f91
unknown <Riyanagwl@gmail.com>
2023-03-23 13:42:54 +0300

    fix linters

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module Api
-  class UsersController < ApplicationController 
+  class UsersController < ApplicationController
     def create
-      user = user.new (users_params)
+      user = user.new(users_params)
       if user.save
-        render json : {success:true}
+        render json: { success: true }
       else
-        render json = {errors:user.errors.full_messages},
-        status : unprocessable_entity
+        render json = { errors: user.errors.full_messages },
+               status: :unprocessable_entity
       end
     end
 
